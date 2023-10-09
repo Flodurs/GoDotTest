90f1eddc97f1aa3e75cfcbbd428986a839c6e32a
Eldhror <147429616+Eldhror@users.noreply.github.com>
2023-10-09 21:38:09 +0200

    Player Controller

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,7 +1,14 @@
 extends CharacterBody3D
 
+var speed: int = 10
+var direction: Vector3
+
 func _process(delta):
+	direction = Vector3.ZERO
 	if Input.is_action_pressed("Forward"):
-		pass
+		direction = $PlayerCamera.look_dir_2d
 	if Input.is_action_pressed("Backward"):
-		pass
+		direction = -$PlayerCamera.look_dir_2d
+	
+	velocity = direction * speed
+	move_and_slide()
--- scenes/Player/player_camera.gd
+++ scenes/Player/player_camera.gd
@@ -1,5 +1,23 @@
 extends Camera3D
 
+var rotation_speed: int = 60
+var camera_direction_3d: Vector3
+var look_dir_2d: Vector3
+
 func _process(delta):
+	camera_direction_3d = ($Marker3D.global_position - global_position).normalized()
+	look_dir_2d[0] = camera_direction_3d[0]
+	look_dir_2d[1] = 0
+	look_dir_2d[2] = camera_direction_3d[2]
+	look_dir_2d = look_dir_2d.normalized()
+	
+	print(look_dir_2d)
+
 	if Input.is_action_pressed("CamUp"):
-		rotate(Vector3(1,0,0), deg_to_rad(1))
+		rotation.x += deg_to_rad(rotation_speed) * delta
+	if Input.is_action_pressed("CamDown"):
+		rotation.x -= deg_to_rad(rotation_speed) * delta
+	if Input.is_action_pressed("CamRight"):
+		rotation.y -= deg_to_rad(rotation_speed) * delta
+	if Input.is_action_pressed("CamLeft"):
+		rotation.y += deg_to_rad(rotation_speed) * delta
