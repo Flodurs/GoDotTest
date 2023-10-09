12925270deb31a836dd9ce448a43281a924127d1
Eldhror <147429616+Eldhror@users.noreply.github.com>
2023-10-09 23:26:26 +0200

    Updated Player Controller

--- scenes/Player/player_camera.gd
+++ scenes/Player/player_camera.gd
@@ -5,13 +5,6 @@ var camera_direction_3d: Vector3
 var look_dir_2d: Vector3
 
 func _process(delta):
-	camera_direction_3d = ($Marker3D.global_position - global_position).normalized()
-	look_dir_2d[0] = camera_direction_3d[0]
-	look_dir_2d[1] = 0
-	look_dir_2d[2] = camera_direction_3d[2]
-	look_dir_2d = look_dir_2d.normalized()
-	
-	print(look_dir_2d)
 
 	if Input.is_action_pressed("CamUp"):
 		rotation.x += deg_to_rad(rotation_speed) * delta
@@ -21,3 +14,10 @@ func _process(delta):
 		rotation.y -= deg_to_rad(rotation_speed) * delta
 	if Input.is_action_pressed("CamLeft"):
 		rotation.y += deg_to_rad(rotation_speed) * delta
+
+func _physics_process(delta):
+	camera_direction_3d = ($Marker3D.global_position - global_position).normalized()
+	look_dir_2d[0] = camera_direction_3d[0]
+	look_dir_2d[1] = 0
+	look_dir_2d[2] = camera_direction_3d[2]
+	look_dir_2d = look_dir_2d.normalized()
