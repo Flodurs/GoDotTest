79cc33690354b90b5cebecf52eb635e4d310ce77
Flodurs <76902177+Flodurs@users.noreply.github.com>
2023-10-10 19:47:29 +0200

    Patrol+Head added to enemy1

--- scenes/enemys/enemy_1.gd
+++ scenes/enemys/enemy_1.gd
@@ -2,8 +2,10 @@ extends CharacterBody3D
 
 
 var gravity = 3
-var rotationspeed = 1
+var rotationspeed = 2
 var target_y_angle = 0
+var movementSpeed = 2
+var targetPoint = 1
 
 func _physics_process(delta):
 	
@@ -13,9 +15,33 @@ func _physics_process(delta):
 	
 	
 func _process(delta):
+	processHeadRotation(delta)
+	processPatrol(delta)
+	
+func processPatrol(delta):
+	
+	if targetPoint == 1:
+		transform.origin.x+=movementSpeed*delta
+	
+	if targetPoint == 0:
+		transform.origin.x-=movementSpeed*delta
+		
+	if transform.origin.x >= 10:
+		targetPoint = 0
+	
+	if transform.origin.x <= -10:
+		targetPoint = 1
+		
+	
+	
+	
+	
+
+	
+func processHeadRotation(delta):
 	var player = get_tree().get_nodes_in_group("Player")[0]
 	
-	var dir = player.transform.origin - transform.origin
+	var dir = player.global_transform.origin - $Head.global_transform.origin
 	var map_xz: Vector2 
 	map_xz.x=dir.x
 	map_xz.y=dir.z
@@ -24,17 +50,20 @@ func _process(delta):
 	
 	
 	var aMap_xz: Vector2
-	aMap_xz.x = transform.basis.x.x
-	aMap_xz.y = transform.basis.x.z
+	aMap_xz.x = $Head.global_transform.basis.x.x
+	aMap_xz.y = $Head.global_transform.basis.x.z
 	
 	var dirAngle = map_xz.angle()
 	var myAngle = aMap_xz.angle()
 	
-	print(dirAngle-myAngle)
+	
 	
 	
 	if abs(dirAngle-myAngle) > 0.01:
-		rotate_y((dirAngle-myAngle)*-0.03)
+		$Head.global_rotate(Vector3(0,1,0),(dirAngle-myAngle)*-rotationspeed*delta)
+		
+		
+	
 	
 	
 	
